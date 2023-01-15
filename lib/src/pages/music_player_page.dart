a4cf7e8b8108ee5200ca40041544ff846a99bb4b
MichiDevelop <michidevelop@gmail.com>
2023-01-15 16:54:03 -0600

    Seccion 13: Clase 177

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/src/widgets/widgets.dart';
 
 class MusicPlayerPage extends StatelessWidget {
   const MusicPlayerPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World'),
+    return Scaffold(
+      body: Column(
+        children: const [
+          CustomAppbar(),
+        ],
       ),
     );
   }
