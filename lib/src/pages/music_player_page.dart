089a37ef5e061283313373c08427eb387fe27988
MichiDevelop <michidevelop@gmail.com>
2023-01-15 17:12:06 -0600

    Seccion 13: Clase 178

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -9,8 +9,70 @@ class MusicPlayerPage extends StatelessWidget {
       body: Column(
         children: const [
           CustomAppbar(),
+          ImagenDiscoDuracion(),
         ],
       ),
+    );
+  }
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+  const ImagenDiscoDuracion({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: const [
+          ImagenDisco()
+          //TODO: Barra progreso
+        ],
+      ),
+    );
+  }
+}
+
+class ImagenDisco extends StatelessWidget {
+  const ImagenDisco({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(200),
+          gradient: const LinearGradient(begin: Alignment.topLeft, colors: [
+            Color(0xff484750),
+            Color(0xff1E1C24),
+          ])),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(200),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              const Image(image: AssetImage('assets/nesuko.png')),
+              Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(100)),
+              ),
+              Container(
+                width: 18,
+                height: 18,
+                decoration: BoxDecoration(
+                    color: const Color(0xff1C1C25),
+                    borderRadius: BorderRadius.circular(100)),
+              )
+            ],
+          )),
     );
   }
 }
