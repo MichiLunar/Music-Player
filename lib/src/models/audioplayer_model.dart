6ba88fb5ac878ead7b8b4c1133825ae9ad7bf9ab
MichiDevelop <michidevelop@gmail.com>
2023-01-15 19:42:52 -0600

    Seccion 13: Clase 186

--- lib/src/models/audioplayer_model.dart
+++ lib/src/models/audioplayer_model.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class AudioPlayerModel with ChangeNotifier {
   bool _playing = false;
 
+  Duration _songDuration = Duration(milliseconds: 0);
+  Duration _current = Duration(milliseconds: 0);
+
+  String get songTotalDuration => printDuration(_songDuration);
+  String get currentSecond => printDuration(_current);
+
+  double get porcentaje => (_songDuration.inSeconds > 0)
+      ? _current.inSeconds / _songDuration.inSeconds
+      : 0;
+
   late AnimationController _controller;
 
   set controller(AnimationController valor) {
@@ -10,4 +20,34 @@ class AudioPlayerModel with ChangeNotifier {
   }
 
   AnimationController get controller => _controller;
+
+  bool get playing => _playing;
+  set playing(bool valor) {
+    _playing = valor;
+    notifyListeners();
+  }
+
+  Duration get songDuration => _songDuration;
+  set songDuration(Duration valor) {
+    _songDuration = valor;
+    notifyListeners();
+  }
+
+  Duration get current => _current;
+  set current(Duration valor) {
+    _current = valor;
+    notifyListeners();
+  }
+
+  String printDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    //return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+    return "$twoDigitMinutes:$twoDigitSeconds";
+  }
 }
